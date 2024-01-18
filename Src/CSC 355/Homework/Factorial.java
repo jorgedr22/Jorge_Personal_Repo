502b6d2700b55b1683917539441894b48ef1b5f8
jorgedr22 <104534378+jorgedr22@users.noreply.github.com>
2024-01-18 11:07:02 -0700

    added Factorial algorithm for HW1

---
+++ Src/CSC 355/Homework/Factorial.java
@@ -0,0 +1,25 @@
+package Homework;
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static int CalculateFactorial(int x){
+        if(x == 0 | x == 1){
+            return 1; // 1! = 0! = 1
+        }
+        else if (x>1){
+            return x * CalculateFactorial(x-1); // Calculate the factorial of x
+        }
+        return -1; // returns -1 for ANY negative integer
+    }
+    public static void main(String[] args) {
+        
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        int factorial = inp.nextInt();
+        
+        System.out.println("The factorial of " + factorial + " is: " + CalculateFactorial(factorial));
+        inp.close();
+    }
+}
